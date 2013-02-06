6b9561eb8e6af74559693f8b598d8c59486555d9
Jesse Falzone <jesse.falzone@gmail.com>
2013-02-06 18:37:05 -0500

    Modified JSON structure for different class sessions. Added scraperLocal.py.

--- scraperLocal.py
+++ scraperLocal.py
@@ -1,156 +1,17 @@
-import time
-import mechanize
-import urllib2
-from pymongo import MongoClient
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
 
-URL = None
-SOUP = None
-YEAR = None
-SEMESTER = None
-SCHOOL = None
-SEMESTERCODE = None
+YEAR = '2013'
+SEMESTER = 'Spring'
+SEMESTERCODE = '01'
+SCHOOL = 'State University of New York at New Paltz'
 
 def main():
+  line = 'CPS'
+  lineCount = 16
+  getClasses(line, lineCount)
 
-  global URL
-  global SOUP
-  global YEAR
-  global SEMESTER
-  global SCHOOL
-  global SEMESTERCODE
-
-  print '\n***********************************'
-  print 'CLASS SCRAPER  JAN-FEB 2013'
-  print '***********************************\n'
-  
-  valid = False
-  while valid == False:
-    number = raw_input('Enter a year:\n')
-    try:
-      YEAR = int(number)
-      YEAR = str(YEAR)
-      valid = True
-    except ValueError:
-      print 'ERROR: Not a valid year!'
-
-  valid = False
-  while valid == False:
-    number = raw_input('Select a semester: 1) Spring 2) Summer 3) Fall 4) Winter\n')
-    try:
-      number = int(number)
-      print ''
-      if number == 1:
-        SEMESTERCODE = '01'
-        SEMESTER = 'Spring'
-        valid = True
-      elif number == 2:
-        SEMESTERCODE = '06'
-        SEMESTER = 'Summer'
-        valid = True
-      elif number == 3:
-        SEMESTERCODE = '09'
-        SEMESTER = 'Fall'
-        valid = True
-      elif number == 4:
-        SEMESTERCODE = '00'
-        SEMESTER = 'Winter'
-        valid = True
-      else:
-        print 'ERROR: Please enter a valid choice'
-    except ValueError:
-        print 'ERROR: Please select a number'        
-  
-  URL = 'https://banner.newpaltz.edu/pls/PROD/bwckzschd.p_dsp_search?p_term=' + YEAR + SEMESTERCODE
-  SOUP = BeautifulSoup(urllib2.urlopen(URL).read(), 'lxml')
-
-  SCHOOL = SOUP.find('h1')  # finds the school name, which is an h1 heading
-  h1s = SOUP.findAll('h1')  # finds the next h1, which is the semester name e.g. Spring 2013
-  SCHOOL = SCHOOL.string
-
-  print SCHOOL.string
-  print h1s[1].string + '\n'
-
-  getSubjects()
-  getPeople()
-  mechSubmit()
-  print '\n...DONE!\n'
-
-#==========================================================
-def getSubjects():
-
-  SubjectFile = open('subjects.txt', 'w')
-  p_subjFile = open('p_subj.txt', 'w')
-  subjects = SOUP.find(id = 'p_subj')
-
-# Get full names of majors, output to file 
-  for row in subjects:
-    if row.string != 'All': 
-      #print row.string
-      SubjectFile.write(row.string)  
-  print 'Wrote subject names to subjects.txt'
-  SubjectFile.close()
-
-# Get 3-letter major codes for form submission
-  for option in subjects.findAll('option'):
-    #print option.get('value')
-    p_subjFile.write(option.get('value')+'\n')
-  print 'Wrote subject codes to p_subj.txt'
-  p_subjFile.close()
-
-#==========================================================
-def getPeople():
-
-  peopleFile = open('people.txt', 'w')
-  p_instrFile = open('p_instr.txt', 'w')
-  people = SOUP.find(id = 'p_instr_pidm')
-
-# Get full names of professors, output to file 
-  for row in people:
-    if row.string != 'All': 
-      #print row.string
-      peopleFile.write(row.string)  
-  print 'Wrote names to people.txt'
-  peopleFile.close()
-
-# Get professors' codes for submission
-  for option in people.findAll('option'):
-    #print option.get('value')
-    p_instrFile.write(option.get('value')+'\n')
-  print 'Wrote instructor ids to p_instr.txt\n'
-  p_instrFile.close()
-
-#==========================================================
-# submit the form for each subject code
-def mechSubmit():
-
-  br = mechanize.Browser()
-  p_subjFile = open('p_subj.txt', 'r')
-  subjFile = open('subjects.txt', 'r')
-  lineCount = 0
-
-  for line in p_subjFile:
-    lineCount += 1
-    line = line.strip()
-    if line == '':
-      lineCount -= 1
-      continue
-    br.open(URL)
-    br.select_form(name='search')  # form name 
-    br['p_subj'] = [line]          # field to select from
-    print 'Submitting ' + line + '... ',
-    response = br.submit()         # submit the form
-    with open(line + '.html', 'w') as outFile:
-      outFile.write(response.read())
-    print 'wrote ' + line + '.html'
-    getClasses(line, lineCount)
-    print 'Sleeping...\n'
-    time.sleep(6)   # wait 6 seconds before submitting the next
-
-  p_subjFile.close()
-
-#==========================================================    
-def getClasses(line, lineCount):
+def getClasses(line,lineCount):
   
   db = MongoClient().test
   
@@ -207,7 +68,7 @@ def getClasses(line, lineCount):
         field = item.string
         if field == None: field = ''
         if field.isdigit() == True and crn != -1:
-          db.courses.insert({'sessions':[{'instructor':instructor1,'time':time1,'days':days1,'location':loc1},{'instructor':instructor2,'time':time2,'days':days2,'location':loc2}],'course':course,'semester':SEMESTER,'section':sec,'year':YEAR,'subject':subject,'title':title,'credits':credits,'link':link,'isRelated':isRelated,'school':SCHOOL,'level':level,'attributes':attrib,'crn':crn,'seatsAvailable':avail})
+          db.courses.insert({'classes':[{'instructor':instructor1,'time':time1,'days':days1,'location':loc1},{'instructor':instructor2,'time':time2,'days':days2,'location':loc2}],'course':course,'semester':SEMESTER,'section':sec,'year':YEAR,'subject':subject,'title':title,'credits':credits,'link':link,'isRelated':isRelated,'school':SCHOOL,'level':level,'attributes':attrib,'crn':crn,'seatsAvailable':avail})
         break
       for item in tds:
           field = item.string 
@@ -305,10 +166,8 @@ def getClasses(line, lineCount):
           counter += 1
 
     # now insert the last class row
-    db.courses.insert({'sessions':[{'instructor':instructor1,'time':time1,'days':days1,'location':loc1},{'instructor':instructor2,'time':time2,'days':days2,'location':loc2}],'course':course,'semester':SEMESTER,'section':sec,'year':YEAR,'subject':subject,'title':title,'credits':credits,'link':link,'isRelated':isRelated,'school':SCHOOL,'level':level,'attributes':attrib,'crn':crn,'seatsAvailable':avail})
+    db.courses.insert({'classes':[{'instructor':instructor1,'time':time1,'days':days1,'location':loc1},{'instructor':instructor2,'time':time2,'days':days2,'location':loc2}],'course':course,'semester':SEMESTER,'section':sec,'year':YEAR,'subject':subject,'title':title,'credits':credits,'link':link,'isRelated':isRelated,'school':SCHOOL,'level':level,'attributes':attrib,'crn':crn,'seatsAvailable':avail})
     print 'done'
 
-
-  
 if __name__ == '__main__':
   main()
